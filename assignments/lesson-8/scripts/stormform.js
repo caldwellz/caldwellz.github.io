99d87b5fe11486265c46edfebe942e5729036b21
Zach Caldwell <zach.caldwell101@gmail.com>
2018-06-16 19:43:46 -0600

    [Lesson 8] Add finishing touches

---
+++ assignments/lesson-8/scripts/stormform.js
@@ -0,0 +1,5 @@
+/* Original concept courtesy of Jason Blazzard's "HTML Form Example Objects" codepen */
+
+function adjustRating(rating) {
+    document.getElementById("storm-severity-rating").innerHTML = rating;
+}
