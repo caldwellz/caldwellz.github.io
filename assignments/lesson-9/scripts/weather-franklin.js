dff99bb7eccb575c78ec68aa9661fa8337fc7a10
Zach Caldwell <zach.caldwell101@gmail.com>
2018-06-23 20:51:17 -0600

    [Lesson 9] Implement Franklin weather summary

--- assignments/lesson-9/scripts/weather-franklin.js
+++ assignments/lesson-9/scripts/weather-franklin.js
@@ -2,21 +2,54 @@
  * Fill in the Franklin Page weather data
  */
 
-// Request the town data JSON
-var reqURL = "https://api.weather.gov/gridpoints/MPX/54,50/forecast";
-var req = new XMLHttpRequest();
-req.open('GET', reqURL);
-req.responseType = 'json';
-req.send();
-
-req.onload = function() {
-    var reqData = req.response;
-    fillSummary(reqData.properties.periods[0]);
-    fillForecast(reqData.properties.periods);
+// Today's high, low, windchill and precip are only in detailed data; forecast is used for everything else.
+// And yeah... I know I could use OpenWeatherMap, but I'd already figured
+// out this API by the time all the instructions for that were discussed.
+var wdetailsURL = "https://api.weather.gov/gridpoints/MPX/54,50";
+var wdetails = new XMLHttpRequest();
+wdetails.open('GET', wdetailsURL);
+wdetails.responseType = 'json';
+wdetails.send();
+wdetails.onload = function() {
+    fillSummaryTemps(wdetails.response.properties);
 }
 
-function fillSummary(data) {
-    return;
+var forecastURL = "https://api.weather.gov/gridpoints/MPX/54,50/forecast";
+var forecast = new XMLHttpRequest();
+forecast.open('GET', forecastURL);
+forecast.responseType = 'json';
+forecast.send();
+forecast.onload = function() {
+    fillSummaryDetails(forecast.response.properties.periods[0]);
+    fillForecast(forecast.response.properties.periods);
+}
+
+function fillSummaryTemps(data) {
+    var eHighTemp  = document.getElementById("highTempNum");
+    var eLowTemp   = document.getElementById("lowTempNum");
+    var ePrecip = document.getElementById("precipitation");
+    var eWindChill = document.getElementById("windchill");
+
+    eHighTemp.textContent  = getFahrenheitStr(data.maxTemperature.values[0].value);
+    eLowTemp.textContent   = getFahrenheitStr(data.minTemperature.values[0].value);
+    ePrecip.textContent    = data.probabilityOfPrecipitation.values[0].value.toString() + "%";
+    eWindChill.textContent = getFahrenheitStr(data.windChill.values[0].value);
+}
+
+function fillSummaryDetails(data) {
+    var eWindSpeed = document.getElementById("windspeed");
+    var eCondShort = document.getElementById("conditions-short");
+    var eCondLong  = document.getElementById("conditions-long");
+    var eCondPict  = document.getElementById("conditions-picture");
+
+    eWindSpeed.textContent = data.windSpeed + " " + data.windDirection;
+    eCondShort.textContent = data.shortForecast;
+    eCondLong.innerHTML    = "<i>Details: </i>" + data.detailedForecast;
+
+    imgElem = document.createElement('img');
+    imgElem.setAttribute("src", data.icon);
+    imgElem.setAttribute("alt", "Weather conditions icon");
+    eCondPict.appendChild(imgElem);
 }
 
 function fillForecast(data) {
@@ -26,4 +59,9 @@ function fillForecast(data) {
         dataElem.innerHTML = data[i].temperature.toString() + "&deg;F";
         tableRow.appendChild(dataElem);
     }
+}
+
+function getFahrenheitStr(celsius) {
+    var f = (1.8 * celsius) + 32;
+    return f.toFixed(0).toString();
 }
