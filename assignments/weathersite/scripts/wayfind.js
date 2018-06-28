c057c91869da643aba71392fe74bcc5cbbd8fc98
Zach Caldwell <zach.caldwell101@gmail.com>
2018-06-27 18:25:48 -0600

    [Lesson 10] Usability: Implement wayfinding

--- assignments/weathersite/scripts/wayfind.js
+++ assignments/weathersite/scripts/wayfind.js
@@ -1,4 +1,16 @@
+// Perform wayfinding highlighting
+function navWayfind() {
+    var pageName = document.getElementsByClassName("page-title")[0].textContent.toLowerCase();
+    var navBar = document.getElementsByClassName("navigation")[0];
 
-function wayfindNav() {
-    
+    // Loop through nav links and see which matches page title
+    for (i = 0; i < navBar.children.length; ++i) {
+        elem = navBar.children[i];
+        // Could do a straight equal test, except the Gallery has a longer
+        // page header title than navbar title
+        if (pageName.includes(elem.textContent.toLowerCase())) {
+            elem.style.backgroundColor = "#666666";
+            return;
+        }
+    }
 }
