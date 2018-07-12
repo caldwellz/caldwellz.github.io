18cbb3d76163eddc97961755d1247c7478cd2968
Zach Caldwell <zach.caldwell101@gmail.com>
2018-07-12 07:37:47 -0600

    [Term Project] Fix some JSLint warnings

--- assignments/term-project/scripts/functions.js
+++ assignments/term-project/scripts/functions.js
@@ -2,20 +2,21 @@
  * Contains general site-wide functionalities such as modularization
  */
 
+"use strict";
 var zc = {};
 
-zc.loadModules = function() {
+zc.loadModules = function () {
     if ('import' in document.createElement('link')) {
         // HTML Imports are supported, we're good to go
         return;
     }
 
     // Otherwise, do it the manual way
-    $("link[rel='import']").each(function() {
+    $("link[rel='import']").each(function () {
         var elem = $(this);
-        var modURL = $(this).attr("href");
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
+        var modURL = elem.attr("href");
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
                 elem.after(this.responseText);
             }
