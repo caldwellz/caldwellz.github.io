870c998bb3523f2897f8b71d44e89f73eb3f2a57
Zach Caldwell <zach.caldwell101@gmail.com>
2018-07-13 21:16:34 -0600

    [Term Project] Fix paths, implement services table

--- assignments/term-project/scripts/functions.js
+++ assignments/term-project/scripts/functions.js
@@ -7,17 +7,18 @@ var zc = {};
 
 zc.loadModules = function () {
     $("link[rel='preload']").each(function () {
-		var elem = $(this);
-		var modURL = elem.attr("href");
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function () {
-			if (this.readyState === 4 && this.status === 200) {
-				// Assume all modules are wrapped in <template>, so get only the stuff inside
-				elem.after(this.responseText);
-			}
-		};
-		xhttp.open("GET", modURL);
-		xhttp.send();
+        var elem = $(this);
+        var modURL = elem.attr("href");
+        var xhttp = new XMLHttpRequest();
+        xhttp.responseType = "text";
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                // Assume all modules are wrapped in <template>, so get only the stuff inside
+                elem.after(this.responseText);
+            }
+        };
+        xhttp.open("GET", modURL);
+        xhttp.send();
     });
 };
 
@@ -34,18 +35,4 @@ zc.wayfind = function () {
     if (elem) {
         elem.addClass("current");
     }
-}
-
-function loadServicesTable() {
-/*
-    request.onload = function () {
-       var response = request.response;
-       $.each(response.services, function(i, item) {
-          var tr = $("<tr>").append(
-          $("<td>").text(item.name),
-          $("<td>").text('$' + item.price.toFixed(2)));
-          $("table").append(tr);
-       });
-   }
-*/
 }
--- assignments/term-project/scripts/services.js
+++ assignments/term-project/scripts/services.js
@@ -3,23 +3,18 @@
  */
 
 
-// Fill in individual service data
-function fillService(service) {
-
+var reqURL = "data/services.json";
+var req = new XMLHttpRequest();
+req.onload = function() {
+   // Add all the services once the JSON finishes loading
+   var response = req.response;
+   $.each(response.services, function(i, item) {
+       var tr = $("<tr>").append(
+           $("<td>").text(item.name),
+           $("<td>").text('$' + item.price.toFixed(2)));
+       $("#services-box table").append(tr);
+   });
 }
-
-
-// Main function to request the services JSON
-function loadServices() {
-   var reqURL = "/data/services.json";
-   var req = new XMLHttpRequest();
-   req.open('GET', reqURL);
-   req.responseType = 'json';
-   req.send();
-   req.onload = function() {
-      // Loop through all the services once the JSON finishes loading
-      var reqData = req.response;
-      for (i = 0; i < reqData['services'].length; i++)
-         fillService(reqData['services'][i]);
-   }
-}
+req.responseType = 'json';
+req.open('GET', reqURL);
+req.send();
