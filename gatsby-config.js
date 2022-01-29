0848582ca4a82777f3c470a3383effe162220674
Zach Caldwell <zach.caldwell101@gmail.com>
2022-01-29 12:37:48 -0700

    Update Gatsby siteUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-      title: `Zach Caldwell - ePortfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Zach Caldwell - ePortfolio`,
+    siteUrl: `http://www.zachc.cf`
   },
   plugins: ["gatsby-plugin-theme-ui", {
     resolve: 'gatsby-plugin-google-analytics',
