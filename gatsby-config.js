33967fff2167fc775b2328ea579cab890b4ec984
Zach Caldwell <zach.caldwell101@gmail.com>
2022-01-29 14:35:22 -0700

    [lint] Run a full linting pass

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `Zach Caldwell - ePortfolio`,
-    siteUrl: `http://www.zachc.cf`
+    title: 'Zach Caldwell - ePortfolio',
+    siteUrl: 'http://www.zachc.cf'
   },
-  plugins: ["gatsby-plugin-theme-ui", {
+  plugins: ['gatsby-plugin-theme-ui', {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
-      "trackingId": "UA-87803288-2"
+      trackingId: 'UA-87803288-2'
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, 'gatsby-plugin-image', 'gatsby-plugin-react-helmet', 'gatsby-plugin-sitemap', 'gatsby-plugin-mdx', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "images",
-      "path": "./src/images/"
+      name: 'images',
+      path: './src/images/'
     },
-    __key: "images"
+    __key: 'images'
   }, {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "pages",
-      "path": "./src/pages/"
+      name: 'pages',
+      path: './src/pages/'
     },
-    __key: "pages"
+    __key: 'pages'
   }]
 };
