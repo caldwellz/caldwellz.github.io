5d1418abb3c7d4be120e6445a5789f877867287c
Zach Caldwell <zach.caldwell101@gmail.com>
2022-01-30 11:24:29 -0700

    Switch to react-bootstrap

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Container } from 'react-bootstrap';
 import Footer from './Footer';
 import Header from './Header';
 import Navbar from './Navbar';
 
 // Pull in main theme - Materia
 import 'bootswatch/dist/materia/bootstrap.min.css';
-import 'bootstrap';
 
 const Layout = ({ children }) => {
   return (
     <div id='layout'>
       <Header />
       <Navbar />
-      <main className='container'>
+      <Container>
         {children}
-      </main>
+      </Container>
       <Footer />
     </div>
   );
