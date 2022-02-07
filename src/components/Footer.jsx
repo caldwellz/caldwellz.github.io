e4cf52eab008a649f9c9093c00a37adb3f41e1ef
Zach Caldwell <zach.caldwell101@gmail.com>
2022-02-06 23:34:11 -0700

    [Footer] Add auto-updating copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Footer = () => {
+const currentYear = new Date().getFullYear();
+
+const Footer = ({ bg, color }) => {
   return (
-    <footer />
+    <footer className={'text-center bg-' + bg + ' text-' + color}>
+      Copyright &copy; {currentYear} Zach Caldwell
+    </footer>
   );
+};
+
+Footer.propTypes = {
+  bg: PropTypes.string,
+  color: PropTypes.string
+};
+
+Footer.defaultProps = {
+  bg: 'dark',
+  color: 'light'
 };
 
 export default Footer;
