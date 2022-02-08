690e9989a15d1761939853b5ab8102c57dc2f5ff
Zach Caldwell <zach.caldwell101@gmail.com>
2022-02-07 21:29:54 -0700

    Implement home (top) page section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,19 @@ import AspectedFrame from '../components/AspectedFrame';
 import Layout from '../components/Layout';
 import Section from '../components/Section';
 
+import profilePicture from '../images/profile-rounded-2022.png';
 import ResumeLinks from '../data/ResumeLinks';
 
 const IndexPage = () => {
   return (
     <Layout>
-      <Section id='home' bgColor='var(--bs-primary)' color='var(--bs-light)' />
+      <Section id='home' bgColor='var(--bs-primary)' color='var(--bs-light)'>
+        <div className='text-center'>
+          <img className='img-fluid' src={profilePicture} alt='This is me. Hello there!' />
+          <h1>Zach Caldwell</h1>
+          <h5>Full-Stack Software Engineer and IT Consultant</h5>
+        </div>
+      </Section>
       <Section title='About Me' />
       <Section title='My Portfolio' />
       <Section title='My Résumé' id='my-resume'>
